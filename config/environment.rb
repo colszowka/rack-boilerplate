417246ec40d8f138ecd7c70c596b8ff74433d357
Christoph Olszowka <christoph@olszowka.de>
2013-11-20 14:09:53 +0100

    Add lib directory to $LOAD_PATH and require the snake_case equivalent of the App constant. This assumes you have that sitting in lib.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 ENV['RACK_ENV'] ||= 'development'
 require 'bundler'
 Bundler.require :default, ENV['RACK_ENV']
+require 'ostruct'
 
 module App
   Config = OpenStruct.new
@@ -16,4 +17,7 @@ module App
   end
 
   Dir[root.join('config/initializers/**/*.rb')].each {|initializer| require initializer }
+  $LOAD_PATH.unshift root.join('lib')
+  # Require the file in load path that matches the snake_case version of your App constant
+  require to_s.gsub(/(\w)([A-Z])/, '\\1_\\2').downcase
 end
