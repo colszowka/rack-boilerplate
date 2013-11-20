6b754cb05036a68f052e6a608acab11fb08aa309
Christoph Olszowka <christoph@olszowka.de>
2013-11-20 13:28:49 +0100

    Application module, heavily inspired by the way Rails does things
    * Give access to the root path of the app, wrapped as a Pathname for easy interaction, via App.root
    * Give access to the rack env programmatically, via App.env
    * Expose a App::Config open_struct for stashing away configuration in an object (`App::Config.foo = 'bar'`)
    * Load all ruby files in config/initializers

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,19 @@
 ENV['RACK_ENV'] ||= 'development'
 require 'bundler'
 Bundler.require :default, ENV['RACK_ENV']
+
+module App
+  Config = OpenStruct.new
+  
+  class << self
+    def root
+      @root ||= Pathname.new(File.dirname(__FILE__)).join('..').expand_path
+    end
+
+    def env
+      @env ||= ENV['RACK_ENV']
+    end
+  end
+
+  Dir[root.join('config/initializers/**/*.rb')].each {|initializer| require initializer }
+end
